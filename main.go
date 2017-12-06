0757e4867aec4f6448b8d9b033f42d919135abe2
Joscelyn James <josce.james7@hotmail.com>
2017-12-06 13:01:51 -0700

    dummy data for users

--- main.go
+++ main.go
@@ -1,49 +1,64 @@
 package main
 
 import (
-    "fmt"
+    // "fmt"
     "log"
 		"net/http"
 		"encoding/json"
-		"time"
+		"strconv"
 
 		"github.com/gorilla/mux"
+		//future use 
 		// "github.com/joscelynjames/rest-api/model"
 )
 
 type User struct {
-    Name      string
-    Admin     bool
-    Created   time.Time
+		ID        int
+		Name      string
+		Email     string
+		Password  string
+		Admin     bool
 }
 
-type Users []User
+var users []User
 
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
+
+	users = append(users, User{ID: 1, Name: "Joscelyn Jmames", Email: "josce.james7@gmail.com", Password: "missy7", Admin: true })
+	users = append(users, User{ID: 2, Name: "Bradford Lamson-Scribner", Email: "bradford@gmail.com", Password: "miles", Admin: true })
+	users = append(users, User{ID: 3, Name: "Cindy Ann James", Email: "CindyAnnJames@gmail.com", Password: "catsrule", Admin: false })
+	users = append(users, User{ID: 4, Name: "Jacqueline Jmames", Email: "jac-attac@gmail.com", Password: "charles123", Admin: false })
+	users = append(users, User{ID: 5, Name: "Joel Jmames", Email: "jjames@gmail.com", Password: "niko80", Admin: false })
+
 	router.HandleFunc("/users", UserIndex)
 	router.HandleFunc("/users/{userId}", UserShow)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome!")
-}
-
 func UserIndex(w http.ResponseWriter, r *http.Request) {
-	users := Users{
-		User{Name: "Write presentation"},
-		User{Name: "Host meetup"},
-	}
-
 	json.NewEncoder(w).Encode(users)
 }
 
 func UserShow(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    userId := vars["userId"]
-    fmt.Fprintln(w, "User show:", userId)
+	// get params from the request
+		vars := mux.Vars(r)
+	// set the param user id to var 
+		intId := vars["userId"]
+	// convert the string to a number 
+		userId, err := strconv.Atoi(intId)
+		if err != nil {
+			panic(err)
+		}
+	// loop over users
+    for _, user := range users {
+			// find the user that id == the param
+        if user.ID == userId {
+            json.NewEncoder(w).Encode(user)
+            return
+        }
+    }
+    return 
 }
