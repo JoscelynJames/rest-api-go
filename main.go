6ff16a902908d5e6759dbb40007ff31d2667d471
Joscelyn James <josce.james7@hotmail.com>
2017-12-08 15:56:59 -0700

    dummy data for coords

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    // "fmt"
     "log"
 		"net/http"
 		"encoding/json"
@@ -11,19 +10,26 @@ import (
 		//future use 
 		// "github.com/joscelynjames/rest-api/model"
 )
-
+// users
 type User struct {
-		ID        int
-		Name      string
-		Email     string
-		Password  string
-		Admin     bool
+		ID        int    `json:"id"` 
+		Name      string `json:"name,"`
+		Email     string `json:"email,"`
+		Password  string `json:"password,"`
+		Admin     bool   `json:"admin,"`
 }
-
 var users []User
 
-func main() {
+// coordinates 
+type Coordinate struct {
+	ID    int    `json:"id"`
+	X 		int 	 `json:"x"`
+	Y 		int 	 `json:"y"`
+	Time  string `json:"time"`
+}
+var coordinates []Coordinate
 
+func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	users = append(users, User{ID: 1, Name: "Joscelyn Jmames", Email: "josce.james7@gmail.com", Password: "missy7", Admin: true })
@@ -32,17 +38,33 @@ func main() {
 	users = append(users, User{ID: 4, Name: "Jacqueline Jmames", Email: "jac-attac@gmail.com", Password: "charles123", Admin: false })
 	users = append(users, User{ID: 5, Name: "Joel Jmames", Email: "jjames@gmail.com", Password: "niko80", Admin: false })
 
-	router.HandleFunc("/users", UserIndex)
-	router.HandleFunc("/users/{userId}", UserShow)
+	coordinates = append(coordinates, Coordinate{ID: 1, X: 5, Y: 8, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 2, X: 5, Y: 2, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 3, X: 3, Y: 4, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 4, X: 2, Y: 4, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 5, X: 2, Y: 6, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 6, X: 3, Y: 5, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 7, X: 4, Y: 6, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 8, X: 4, Y: 7, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 9, X: 5, Y: 5, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 10, X: 6, Y: 7, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 11, X: 6, Y: 8, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 12, X: 7, Y: 7, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 13, X: 7, Y: 6, Time: "December 23 20017" })
+	coordinates = append(coordinates, Coordinate{ID: 14, X: 10, Y: 5, Time: "December 23 20017" })
+
+	router.HandleFunc("/users", allUsers)
+	router.HandleFunc("/users/{userId}", oneUser)
+	router.HandleFunc("/coords", allCoords)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func UserIndex(w http.ResponseWriter, r *http.Request) {
+func allUsers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
-func UserShow(w http.ResponseWriter, r *http.Request) {
+func oneUser(w http.ResponseWriter, r *http.Request) {
 	// get params from the request
 		vars := mux.Vars(r)
 	// set the param user id to var 
@@ -62,3 +84,9 @@ func UserShow(w http.ResponseWriter, r *http.Request) {
     }
     return 
 }
+
+func allCoords(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(coordinates)
+}
+
+
